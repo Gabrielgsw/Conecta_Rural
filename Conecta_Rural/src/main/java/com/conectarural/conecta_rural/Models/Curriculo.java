60910bb94101b197fe8ca96415b1a33de4f77811
Gabriel Germano <113439146+Gabrielgsw@users.noreply.github.com>
2024-05-12 21:27:11 -0300

    Update Curriculo.java

--- Conecta_Rural/src/main/java/com/conectarural/conecta_rural/Models/Curriculo.java
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/Models/Curriculo.java
@@ -2,19 +2,26 @@ package com.conectarural.conecta_rural.Models;
 
 public class Curriculo {
 
-    private String Resumo;
+    private String resumo;
     private Integer periodoAtual;
-
+    private File curriculoArquivo;
+    private estudanteProjeto projeto;
 
     public Curriculo(){
     }
-
+    
+    public Curriculo(String resumo, Integer periodoAtual, estudanteProjeto projeto){
+        this.resumo = resumo;
+        this.periodoAtual = periodoAtual;
+        this.projeto = projeto;
+    }
+    
     public String getResumo() {
-        return Resumo;
+        return resumo;
     }
 
     public void setResumo(String resumo) {
-        Resumo = resumo;
+        this.resumo = resumo;
     }
 
     public Integer getPeriodoAtual() {
@@ -25,10 +32,25 @@ public class Curriculo {
         this.periodoAtual = periodoAtual;
     }
 
+    public estudanteProjeto getProjeto() {
+        return projeto;
+    }
+
+    public void setProjeto(estudanteProjeto projeto){
+        this.projeto = projeto;  
+    }       
+    
+    public File getCurriculoArquivo() {
+        return curriculoArquivo;
+    }
+    public void setCurriculoArquivo(File curriculoArquivo){
+        this.curriculoArquivo = curriculoArquivo;
+    }
+    
     @Override
     public String toString() {
         return "Curriculo{" +
-                "Resumo='" + Resumo + '\'' +
+                "Resumo='" + resumo + '\'' +
                 ", periodoAtual=" + periodoAtual +
                 '}';
     }
