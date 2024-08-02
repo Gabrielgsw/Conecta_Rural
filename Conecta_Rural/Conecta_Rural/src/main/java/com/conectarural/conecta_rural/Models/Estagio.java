ebf31d13f6ca9e5012566bddff5889ec7a60508f
JuanPabollo <pabollo61@gmail.com>
2024-08-02 14:35:18 -0300

    Restaurando projeto

--- Conecta_Rural/Conecta_Rural/src/main/java/com/conectarural/conecta_rural/Models/Estagio.java
+++
@@ -1,9 +0,0 @@
-package com.conectarural.conecta_rural.Models;
-
-import java.time.LocalDateTime;
-
-public class Estagio extends Atividade{
-    public Estagio(String nome, String resumo, String area, String tecnologias, LocalDateTime data) {
-        super(nome, resumo, area, tecnologias, data);
-    }
-}
--- Conecta_Rural/Conecta_Rural/src/main/java/com/conectarural/conecta_rural/Models/Trabalho.java
+++
@@ -1,9 +0,0 @@
-package com.conectarural.conecta_rural.Models;
-
-import java.time.LocalDateTime;
-
-public class Trabalho extends Atividade{
-    public Trabalho(String nome, String resumo, String area, String tecnologias, LocalDateTime data) {
-        super(nome, resumo, area, tecnologias, data);
-    }
-}
