dadd9e5f8532fc200adfe077c7ade89a96ff49e1
DanielFeitosadeLima <danielfeitosalimaptf1@gmail.com>
2024-08-14 08:54:59 -0300

    CandidaturaRepositoryAdicionado

--- Conecta_Rural/src/main/java/com/conectarural/conecta_rural/dados/CandidaturasRepository.java
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/dados/CandidaturasRepository.java
@@ -1,4 +1,79 @@
 package com.conectarural.conecta_rural.dados;
+import com.conectarural.conecta_rural.Exceptions.ElementoNaoExistenteException;
+import com.conectarural.conecta_rural.Exceptions.ElementosNaoIguaisException;
+import com.conectarural.conecta_rural.Models.Candidatura;
+import java.util.List;
+import java.util.ArrayList;
 
-public class CandidaturasRepository {
+
+public abstract class CandidaturasRepository implements IRepository<Candidatura> {
+
+    private List<Candidatura> listaDeCandidaturas;
+
+    public CandidaturasRepository() {
+        this.listaDeCandidaturas = new ArrayList<>();
+    }
+
+
+    public void adicionar(Candidatura c) throws ElementoNaoExistenteException {
+
+        if (!listaDeCandidaturas.contains(c)) {
+            listaDeCandidaturas.add(c);
+        } else {
+            throw new ElementoNaoExistenteException();
+        }
+    }
+
+    public List<Candidatura> listar() {
+
+        List<Candidatura> candidaturas = new ArrayList<>();
+
+        for (Candidatura candidatura : listaDeCandidaturas) {
+            candidaturas.add(candidatura);
+        }
+        return candidaturas;
+    }
+
+    public void remover(Candidatura c) throws ElementoNaoExistenteException {
+
+        boolean existe = false;
+
+        for (Candidatura candidatura : listaDeCandidaturas) {
+
+            if (c.getCandidato() == candidatura.getCandidato()) {
+
+                existe = true;
+
+            }
+        }
+
+        if (existe) {
+            listaDeCandidaturas.remove(c);
+        } else {
+
+            throw new ElementoNaoExistenteException();
+        }
+
+    }
+
+    public void atualizar(Candidatura antigo, Candidatura novo) throws ElementoNaoExistenteException, ElementosNaoIguaisException {
+        if(antigo.getCandidato().getCpf() == novo.getCandidato().getCpf()) {
+
+            boolean existe = listaDeCandidaturas.contains(antigo);
+            if(existe){
+                for(Candidatura candidatura : listaDeCandidaturas){
+                    candidatura = novo;
+                }
+            }else{
+                throw new ElementoNaoExistenteException();
+            }
+
+        }else{
+            throw new ElementosNaoIguaisException();
+        }
+
+
+
+    }
+
 }
