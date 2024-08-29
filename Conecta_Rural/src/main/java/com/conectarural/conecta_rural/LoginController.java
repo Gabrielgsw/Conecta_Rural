6229654d79b285c027ef928352ef6629d8a19309
Gabrielgsw <ggermanow279@gmail.com>
2024-08-29 12:11:00 -0300

    adicionando telas buscarEstudantes,editarvagas,visualizarcandEmp,visVagas

--- Conecta_Rural/src/main/java/com/conectarural/conecta_rural/LoginController.java
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/LoginController.java
@@ -1,9 +1,11 @@
 package com.conectarural.conecta_rural;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,16 +16,24 @@ import java.io.IOException;
 public class LoginController {
 
     @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+    @FXML
     private Button cadastroBT;
 
     @FXML
-    public void onCadastroBTaction() throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("EmpresaCadastro.fxml"));
-        Scene mscene = new Scene(root, 600, 400 );
-        Stage nstage = new Stage();
-        nstage.setScene(mscene);
-        nstage.show();
-        System.out.println("Cadastro BT");
+    public void onCadastroBTaction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("EmpresaCadastro.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        //Scene mscene = new Scene(root, 600, 400 );
+       // Stage nstage = new Stage();
+       // nstage.setScene(mscene);
+        //nstage.show();
+        //System.out.println("Cadastro BT");
     }
 
 
