414301de8abd805a37ed4e9a01a800a62335247a
DanielFeitosadeLima <danielfeitosalimaptf1@gmail.com>
2024-08-31 19:04:37 -0300

    CurriculoController+BuscarVagasControllerAdicionados

---
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/negocio/BuscarVagasController.java
@@ -0,0 +1,78 @@
+package com.conectarural.conecta_rural.negocio;
+
+import com.conectarural.conecta_rural.HelloApplication;
+import com.conectarural.conecta_rural.models.Empresa;
+import com.conectarural.conecta_rural.models.Vaga;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class BuscarVagasController {
+
+    @FXML
+    private Stage stage;
+
+    @FXML
+    private Scene scene;
+
+    @FXML
+    private Button botaoBuscar;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private TableView<Vaga> table;
+
+    @FXML
+    private TableColumn<Vaga, Empresa> colunaEmpresa;
+
+    @FXML
+    private TableColumn<Vaga, String> colunaDescricaoVaga;
+
+    @FXML
+    private TableColumn<Vaga, Double> colunaRemuneracao;
+
+    @FXML
+    private TableColumn<Vaga, Integer> colunaQuantidadeVagas;
+
+    @FXML
+    private TableColumn<Vaga, String> colunaNomeVaga;
+
+    @FXML
+    private TableColumn<Vaga, Double> colunaValorVaga;
+
+    @FXML
+    private AnchorPane BuscarVagasAnchorPane;
+
+    @FXML
+    void acaoBotaoBuscarVagas(ActionEvent event) throws IOException {
+        System.out.println("acaoBotaoBuscarVagas");
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaBuscarVagas.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    void acaoBotaoVoltar(ActionEvent event) throws IOException{
+        System.out.println("acaoBotaoVoltar");
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaPrincipalEstudante.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+}
---
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/negocio/CurriculoController.java
@@ -0,0 +1,73 @@
+package com.conectarural.conecta_rural.negocio;
+
+import com.conectarural.conecta_rural.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class CurriculoController {
+
+    @FXML
+    private Stage stage;
+
+    @FXML
+    private Scene scene;
+
+    @FXML
+    private Button BotaoGerarCurriculo;
+
+    @FXML
+    private Button BotaoVoltar;
+
+    @FXML
+    private AnchorPane curriculoAnchorPane;
+
+    @FXML
+    private TextField nomeEstudanteCurriculo;
+
+    @FXML
+    private TextField ResumoEstudanteCurriculo;
+
+    @FXML
+    private TextField periodoAtualEstudanteCurriculo;
+
+    @FXML
+    private TextField cursoEstudanteCurriculo;
+
+    @FXML
+    private TextField atividadesEstudanteCurriculo;
+
+    @FXML
+    void acaoBotaoGerarCurriculo(ActionEvent event) throws IOException {
+        System.out.println("acaoBotaoGerarCurriculo");
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaCurriculo.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void acaoBotaoVoltar(ActionEvent event) throws IOException{
+        System.out.println("acaoBotaoVoltar");
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaPerfilEstudante.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+}
