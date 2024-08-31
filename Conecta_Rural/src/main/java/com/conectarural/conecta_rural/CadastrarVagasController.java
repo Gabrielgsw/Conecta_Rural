2468326b3bf40922cfb5813a9547de66ec723f2d
JuanPabollo <pabollo61@gmail.com>
2024-08-31 15:00:58 -0300

    Adiciona a acao dos botoes da tela principal empresa no controlador.

--- Conecta_Rural/src/main/java/com/conectarural/conecta_rural/CadastrarVagasController.java
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/CadastrarVagasController.java
@@ -1,4 +1,72 @@
 package com.conectarural.conecta_rural;
 
+import com.conectarural.conecta_rural.models.RegimeContratacao;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CadastrarVagasController {
+
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+
+    @FXML
+    private Button cadastrarVagaBtn;
+
+    @FXML
+    private TextField codigoVagaField;
+
+    @FXML
+    private TextArea descricaoVagaArea;
+
+    @FXML
+    private TextField nomeDaVagaField;
+
+    @FXML
+    private TextField numeroDeVagasField;
+
+    @FXML
+    private MenuItem regimeClt;
+
+    @FXML
+    private MenuItem regimeEstagio;
+
+    @FXML
+    private MenuItem regimeFreelancer;
+
+    @FXML
+    private MenuItem regimePj;
+
+    private MenuButton regimeContratacaoMenu;
+
+    @FXML
+    private MenuItem regimeTemporario;
+
+    @FXML
+    private TextField valorRemuneracaoField;
+
+    @FXML
+    public void onCadastroarVagaBtnaction(ActionEvent event) throws IOException {
+       /* Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaCadastrarVagas.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();*/
+
+        int codigoVaga = Integer.parseInt(codigoVagaField.getText());
+        String descricaoVaga = descricaoVagaArea.getText();
+        String nomeDaVaga = nomeDaVagaField.getText();
+        String numeroDeVaga = numeroDeVagasField.getText();
+
+
+    }
 }
--- Conecta_Rural/src/main/java/com/conectarural/conecta_rural/PrincipalEmpresaController.java
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/PrincipalEmpresaController.java
@@ -1,4 +1,96 @@
 package com.conectarural.conecta_rural;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class PrincipalEmpresaController {
+
+    @FXML
+    private Stage stage;
+
+    @FXML
+    private Scene scene;
+
+    @FXML
+    private Button buscarEstudantesBtn;
+
+    @FXML
+    private Button cadastrarNovaVagaBtn;
+
+    @FXML
+    private Button meuPerfilBtn;
+
+    @FXML
+    private Label nomeDaEmpresaLabel;
+
+    @FXML
+    private Button paginaAnteriorBtn;
+
+    @FXML
+    private Button visualizarMinhasVagasBtn;
+
+    @FXML
+    public void onCadastrarNovaVagaBtnBtnAction(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaCadastrarVagas.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @FXML
+    public void onBuscarEstudantesBtnAction(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaBuscarEstudante.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @FXML
+    public void onPaginaAnteriorBtnAction(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("Login.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @FXML
+    public void onVisualizarMinhasVagasBtnAction(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaVisualizarCandidaturasEmpresa.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @FXML
+    public void onMeuPerfilBtnAction(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaPerfilEmpresa.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
 }
