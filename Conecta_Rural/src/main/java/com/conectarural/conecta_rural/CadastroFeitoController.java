0c003e6a80f5334cb81b01965a003a09ca1ca075
Antônio Chaves Patriota <antoniochavesacp@gmail.com>
2024-08-31 13:52:45 -0300

    telasAtualizacaoAntonio

---
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/CadastroFeitoController.java
@@ -0,0 +1,16 @@
+package com.conectarural.conecta_rural;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CadastroFeitoController {
+
+    @FXML
+    private Button botaoRetorno;
+
+    @FXML
+    void acaoBotaoRetorno(ActionEvent event) {
+        System.out.println("acaoBotaoRetorno");
+    }
+
+}
