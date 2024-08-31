859b4bb3b5392f9d1c5d3400fff62726d81b819a
Antônio Chaves Patriota <antoniochavesacp@gmail.com>
2024-08-31 16:57:36 -0300

    VerCandidaturaControler->Antonio

--- Conecta_Rural/src/main/java/com/conectarural/conecta_rural/PrincipalEstudanteController.java
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/PrincipalEstudanteController.java
@@ -2,11 +2,17 @@ package com.conectarural.conecta_rural;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PrincipalEstudanteController {
 
@@ -35,28 +41,53 @@ public class PrincipalEstudanteController {
     private TextField nomeEstudante;
 
     @FXML
-    void acaoBotaoBuscarVagas(ActionEvent event) {
+    void acaoBotaoBuscarVagas(ActionEvent event) throws IOException {
         System.out.println("acaoBotaoBuscarVagas");
+        Parent parent = FXMLLoader.load(getClass().getResource("TelaBuscarVagas.fxml"));
+        Scene mscene = new Scene(parent, 600, 400 );
+        Stage nstage = new Stage();
+        nstage.setScene(mscene);
+        nstage.show();
     }
 
     @FXML
-    void acaoBotaoCandidaturas(ActionEvent event) {
+    void acaoBotaoCandidaturas(ActionEvent event) throws IOException{
         System.out.println("acaoBotaoCandidaturas");
+        Parent parent = FXMLLoader.load(getClass().getResource("VisualizarCandidaturas.fxml"));
+        Scene mscene = new Scene(parent, 600, 400 );
+        Stage nstage = new Stage();
+        nstage.setScene(mscene);
+        nstage.show();
     }
 
     @FXML
-    void acaoBotaoSairEstudante(ActionEvent event) {
+    void acaoBotaoSairEstudante(ActionEvent event) throws IOException{
         System.out.println("acaoBotaoSairEstudante");
+        Parent parent = FXMLLoader.load(getClass().getResource("EscolherPerfil.fxml"));
+        Scene mscene = new Scene(parent, 600, 400 );
+        Stage nstage = new Stage();
+        nstage.setScene(mscene);
+        nstage.show();
     }
 
     @FXML
-    void acaoBotaoVerPerfil(ActionEvent event) {
+    void acaoBotaoVerPerfil(ActionEvent event) throws IOException{
         System.out.println("acaoBotaoVerPerfil");
+        Parent parent = FXMLLoader.load(getClass().getResource("PerfilEstudante.fxml"));
+        Scene mscene = new Scene(parent, 600, 400 );
+        Stage nstage = new Stage();
+        nstage.setScene(mscene);
+        nstage.show();
     }
 
     @FXML
-    void acaoBotaoVerVagasAbertas(ActionEvent event) {
+    void acaoBotaoVerVagasAbertas(ActionEvent event) throws IOException{
         System.out.println("acaoBotaoVerVagasAbertas");
+        Parent parent = FXMLLoader.load(getClass().getResource("TelaVisualizarVagasAbertas.fxml"));
+        Scene mscene = new Scene(parent, 600, 400 );
+        Stage nstage = new Stage();
+        nstage.setScene(mscene);
+        nstage.show();
     }
 
 
--- Conecta_Rural/src/main/java/com/conectarural/conecta_rural/VerCandidaturaController.java
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/VerCandidaturaController.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
