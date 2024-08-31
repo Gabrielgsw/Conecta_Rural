db764c6a4a63bd81f80098dc7ce98b712e812afe
JuanPabollo <pabollo61@gmail.com>
2024-08-31 15:15:51 -0300

    Adiciona a acao dos botoes da tela perfil empresa no controlador.

--- Conecta_Rural/src/main/java/com/conectarural/conecta_rural/PerfilEmpresaController.java
+++ Conecta_Rural/src/main/java/com/conectarural/conecta_rural/PerfilEmpresaController.java
@@ -24,11 +24,18 @@ public class PerfilEmpresaController {
 
     @FXML
     public void onvoltarTelaPrincipalBtn(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaPerfilEmpresa.fxml"));
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("TelaPrincipalEmpresa.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    public void oneditarPerfilBtn(ActionEvent event) throws IOException {
+
+        System.out.println("editarPerfilBtn");
+
     }
 
 }
